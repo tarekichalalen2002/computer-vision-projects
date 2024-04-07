c3a58e346309328497c2a03c04edbdd21d41fe52
tarekichalalen2002 <t_ichalalen@estin.dz>
2024-04-07 23:25:51 +0200

    first commit

---
+++ gesture-volume-control/main.py
@@ -0,0 +1,61 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+volumeRange = volume.GetVolumeRange()
+minVol = volumeRange[0]
+maxVol = volumeRange[1]
+
+################################################################
+
+wCam, hCam = 640, 480
+
+################################################################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.HandDetector(detectionCon=0.7)
+
+while True:
+    success , img = cap.read()
+    img = detector.findHands(img)
+    lmlist = detector.findPosition(img, draw=False)
+
+    if len(lmlist) != 0:
+        x1, y1 = lmlist[4][1], lmlist[4][2]
+        x2, y2 = lmlist[8][1], lmlist[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+        cv2.putText(img, f'line length: {int(length)}', (40, 250), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        if length < 50:
+            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
+        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        volume.SetMasterVolumeLevel(vol, None)
+        
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
